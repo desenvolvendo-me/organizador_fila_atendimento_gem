21d0ab53e5b9d18fe58098f7b67efe41239a3057
Marco Castro <marcodotcastro@gmail.com>
2024-05-25 09:26:30 -0300

    feat: Estatísticas de Simulação

--- lib/organizador_fila_atendimento_gem.rb
+++ lib/organizador_fila_atendimento_gem.rb
@@ -4,5 +4,21 @@ require_relative "organizador_fila_atendimento_gem/version"
 
 module OrganizadorFilaAtendimentoGem
   class Error < StandardError; end
-  # Your code goes here...
+
+  class SimuladorFila
+    def initialize
+      @random = Random.new
+    end
+
+    def tempo_chegada_cliente
+      # Suponha que queremos simular tempos de chegada de clientes entre 1 e 10 minutos
+      @random.rand(1..10)
+    end
+
+    def tempo_atendimento
+      # Suponha que queremos simular tempos de atendimento entre 5 e 20 minutos
+      @random.rand(5..20)
+    end
+  end
+
 end
