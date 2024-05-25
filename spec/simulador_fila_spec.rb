5a61a1a21241f90cc615e4707ad8071cfbc967a3
Marco Castro <marcodotcastro@gmail.com>
2024-05-25 09:27:34 -0300

    test: Simulação de Fila

--- spec/simulador_fila_spec.rb
+++ spec/simulador_fila_spec.rb
@@ -1,18 +1,33 @@
 # spec/simulador_fila_spec.rb
 
-require 'organizador_fila_atendimento_gem'
+require "organizador_fila_atendimento_gem"
 
 RSpec.describe OrganizadorFilaAtendimentoGem::SimuladorFila do
-  describe '#tempo_chegada_cliente' do
-    it 'gera um tempo de chegada de cliente entre 1 e 10 minutos' do
+  describe "#adicionar_cliente" do
+    it "adiciona um cliente à fila" do
+      simulador = OrganizadorFilaAtendimentoGem::SimuladorFila.new
+      expect { simulador.adicionar_cliente(5) }.to change { simulador.instance_variable_get(:@fila).size }.from(0).to(1)
+    end
+  end
+
+  describe "#processar_atendimento" do
+    it "remove o cliente da fila e calcula o tempo de espera" do
+      simulador = OrganizadorFilaAtendimentoGem::SimuladorFila.new
+      simulador.adicionar_cliente(5)
+      expect { simulador.processar_atendimento(10) }.to output("Cliente atendido após 5 unidades de tempo.\n").to_stdout
+    end
+  end
+
+  describe "#tempo_chegada_cliente" do
+    it "gera um tempo de chegada de cliente entre 1 e 10 minutos" do
       simulador = described_class.new
       tempo_chegada = simulador.tempo_chegada_cliente
       expect(tempo_chegada).to be_between(1, 10)
     end
   end
 
-  describe '#tempo_atendimento' do
-    it 'gera um tempo de atendimento entre 5 e 20 minutos' do
+  describe "#tempo_atendimento" do
+    it "gera um tempo de atendimento entre 5 e 20 minutos" do
       simulador = described_class.new
       tempo_atendimento = simulador.tempo_atendimento
       expect(tempo_atendimento).to be_between(5, 20)
